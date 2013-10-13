fb5ec942aefa3f835859b27e0220925ebcb83641
Karina <karinazelko@hotmail.com>
2013-10-13 10:20:58 -0300

    Cursos

---
+++ UI.Desktop/Cursos.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace UI.Desktop
+{
+    partial class Cursos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tcCursos = new System.Windows.Forms.ToolStripContainer();
+            this.tcCursos.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tcCursos
+            // 
+            // 
+            // tcCursos.ContentPanel
+            // 
+            this.tcCursos.ContentPanel.Size = new System.Drawing.Size(284, 237);
+            this.tcCursos.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tcCursos.Location = new System.Drawing.Point(0, 0);
+            this.tcCursos.Name = "tcCursos";
+            this.tcCursos.Size = new System.Drawing.Size(284, 262);
+            this.tcCursos.TabIndex = 0;
+            this.tcCursos.Text = "toolStripContainer1";
+            // 
+            // Cursos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.tcCursos);
+            this.Name = "Cursos";
+            this.Text = "Cursos";
+            this.tcCursos.ResumeLayout(false);
+            this.tcCursos.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStripContainer tcCursos;
+    }
+}
