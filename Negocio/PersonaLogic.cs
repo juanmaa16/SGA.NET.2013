584eafc8f850d942a6a4add75fbdfe3393e794cc
Juan Manuel Ortiz <juanmaortyz@gmail.com>
2013-10-14 11:36:54 -0300

    Negocio: PersonaLogic

---
+++ Negocio/PersonaLogic.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Entidades;
+using Data.Database;
+
+namespace Negocio
+{
+    public class PersonaLogic : BusinessLogic
+    {
+        PersonaAdapter pData;
+
+        public PersonaLogic()
+        {
+            this.pData = new PersonaAdapter();
+        }
+
+        public PersonaAdapter PersonaData
+        {
+            get;
+            set;
+        }
+
+        public List<Persona> GetAll()
+        {
+            return pData.GetAll();
+        }
+
+        public Persona GetOne(int ID)
+        {
+            return pData.GetOne(ID);
+        }
+
+        public void Delete(int ID)
+        {
+            pData.Delete(ID);
+        }
+
+        public void Save(Persona persona)
+        {
+            pData.Save(persona);
+        }
+    }
+}
